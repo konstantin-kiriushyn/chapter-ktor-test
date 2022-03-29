8fc9d461e84e4e2f775dc41a04d4b31078efe6d5
Kshitij Jain <kshitijjain@6771GLOBAL.local>
2022-03-29 17:00:48 +0200

    Improve the Token Retrieval Logic

--- src/main/kotlin/com/hf/rcs/client/HfClient.kt
+++ src/main/kotlin/com/hf/rcs/client/HfClient.kt
@@ -1,6 +1,5 @@
 package com.hf.rcs.client
 
-import com.hf.rcs.data.AuthResponse
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.auth.*
@@ -10,11 +9,8 @@ import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 class HfClient {
-
 
     private val client = HttpClient(CIO) {
         install(Auth) {
@@ -27,13 +23,11 @@ class HfClient {
     }
     private val url = ""
 
-    var token: String? = null
-
     @OptIn(InternalAPI::class)
-    suspend fun authorize() {
+    suspend fun authorize(): HttpResponse {
         val path = "/login"
 
-        val response = client.post("$url$path") {
+        return client.post("$url$path") {
             parameter("country", "US")
             parameter("locale", "en-US")
 
@@ -42,29 +36,16 @@ class HfClient {
                 append("password", "")
             })
         }
-
-        token = if (response.status == HttpStatusCode.OK) {
-            val format = Json {
-                isLenient = true
-                ignoreUnknownKeys = true
-            }
-            val authResponse = format.decodeFromString<AuthResponse>(response.bodyAsText())
-            authResponse.accessToken
-        } else {
-            null
-        }
     }
 
-    suspend fun search(): HttpResponse {
-
+    suspend fun search(token: String? = null): HttpResponse {
         val path = "/recipes/recipes/6203c4328c77620a052d7425"
         return client.get("$url$path") {
-//            parameter("weeks", "2")
-//            parameter("take", 20)
             parameter("country", "US")
             parameter("locale", "en-US")
-            header("Authorization", "Bearer $token")
+            token?.let {
+                header("Authorization", "Bearer $token")
+            }
         }
     }
-
 }
--- src/main/kotlin/com/hf/rcs/plugins/Routing.kt
+++ src/main/kotlin/com/hf/rcs/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package com.hf.rcs.plugins
 
 import com.hf.rcs.client.HfClient
+import com.hf.rcs.data.AuthResponse
+import io.ktor.client.plugins.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 fun Application.configureRouting() {
 
@@ -32,14 +37,31 @@ fun Application.configureRouting() {
 }
 
 val hfClient = HfClient()
+var token: String? = null
 
 fun Route.hfRouting() {
     get("/recipes") {
-        if (hfClient.token == null) {
-            hfClient.authorize()
+        val response: HttpResponse = try {
+            hfClient.search(token)
+        } catch (exception: ClientRequestException) {
+            authorize()
+            hfClient.search(token)
         }
-        val result = hfClient.search()
-        call.respondText { result.bodyAsText() }
+        call.respondText { response.bodyAsText() }
+    }
+}
+
+suspend fun authorize() {
+    val authResponse = hfClient.authorize()
+    token = if (authResponse.status == HttpStatusCode.OK) {
+        val format = Json {
+            isLenient = true
+            ignoreUnknownKeys = true
+        }
+        val response = format.decodeFromString<AuthResponse>(authResponse.bodyAsText())
+        response.accessToken
+    } else {
+        null
     }
 }
 

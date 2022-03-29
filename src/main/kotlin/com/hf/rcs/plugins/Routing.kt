d0754ea4cda33471a81eace6c0a73fa48ab91b9c
Kshitij Jain <kshitijjain@6771GLOBAL.local>
2022-03-29 12:47:21 +0200

    Add Delete and Put Operations in API

--- src/main/kotlin/com/hf/rcs/plugins/Routing.kt
+++ src/main/kotlin/com/hf/rcs/plugins/Routing.kt
@@ -20,6 +20,7 @@ fun Route.rcsRouting() {
     route("/rcs") {
 
         get {
+            simpleFeatures.sortBy { it.id }
             call.respond(simpleFeatures)
         }
 
@@ -30,6 +31,36 @@ fun Route.rcsRouting() {
                 call.respondText("New Feature Added $simpleFeatures", status = HttpStatusCode.Created)
             } catch (exception: ContentTransformationException) {
                 call.respondText("Error adding feature ${exception.message}", status = HttpStatusCode.BadRequest)
+            }
+        }
+
+        delete("{id}") {
+            val featureToDelete = call.parameters["id"] ?: return@delete call.respondText(
+                "Parameter is missing",
+                status = HttpStatusCode.BadRequest
+            )
+            val feature = simpleFeatures.find { it.id == featureToDelete }
+            if (feature != null) {
+                simpleFeatures.remove(feature)
+                call.respondText("Feature was deleted $simpleFeatures", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Feature by Id not found $simpleFeatures", status = HttpStatusCode.NotFound)
+            }
+        }
+
+        put {
+            try {
+                val updateFeature = call.receive<SingleFeature>()
+                val foundFeature = simpleFeatures.find { it.id == updateFeature.id }
+                if (foundFeature != null) {
+                    simpleFeatures.remove(foundFeature)
+                    simpleFeatures.add(updateFeature)
+                    call.respondText("Feature Updated $simpleFeatures", status = HttpStatusCode.Accepted)
+                } else {
+                    call.respondText("Feature by Id not found $simpleFeatures", status = HttpStatusCode.NotFound)
+                }
+            } catch (exception: ContentTransformationException) {
+                call.respondText("Bad Request ${exception.message}", status = HttpStatusCode.BadRequest)
             }
         }
     }
